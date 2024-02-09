a45632a8e62320e41defaf3727c4272ea2f12304
kradhenz <tomas25498@gmail.com>
2024-02-09 20:56:35 -0300

    working in validation function

--- assets/js/ex1.js
+++ assets/js/ex1.js
@@ -1,64 +1,94 @@
+function validation(name, subject, msg) {
+    let success = true;
+    let validate = /[a-zA-Z]/gim;
+
+    if (validate.test(name) == false) {
+        document.querySelector(".nameError").innerHTML = "Ingrese un nombre válido."
+        success = false;
+    };
+
+    if (validate.test(subject) == false) {
+        document.querySelector(".subjectError").innerHTML = "Ingrese un nombre válido."
+        success = false;
+    };
+
+    if (validate.test(msg) == false) {
+        document.querySelector(".msgError").innerHTML = "Ingrese un nombre válido."
+        success = false;
+    };
+
+    return success;
+}
+/* 
+    -->   START   <-- 
+*/
 var myBtn = document.getElementById("send");
 myBtn.addEventListener('click', function () {
-    var name = document.querySelector("#name");
-    var subject = document.querySelector("#subject");
-    var msg = document.querySelector("#msg");
-    // FULL SUCCESS CASE
-    if (name.value != "" && subject.value != "" && msg.value != "") {
-        document.querySelector(".validate").innerHTML = "¡Mensaje enviado con éxito!";
-        document.querySelector(".nameError").innerHTML = "";
-        document.querySelector(".subjectError").innerHTML = "";
-        document.querySelector(".msgError").innerHTML = "";
-    } 
-    // TWO SUCCES CONDITIONAL
-    // NAME & SUBJECT SUCCESS, MSG FAILURE
-    else if (name.value != "" && subject.value != "" && msg.value == "") {
-        document.querySelector(".msgError").innerHTML = "El mensaje es requerido.";
-        document.querySelector(".nameError").innerHTML = "";
-        document.querySelector(".subjectError").innerHTML = "";
-        document.querySelector(".validate").innerHTML = "";
+    let name = document.querySelector("#name");
+    let subject = document.querySelector("#subject");
+    let msg = document.querySelector("#msg");
+
+    if (validation(name) == true) {
+        // FULL SUCCESS CASE
+        if (name.value != "" && subject.value != "" && msg.value != "") {
+            document.querySelector(".validate").innerHTML = "¡Mensaje enviado con éxito!";
+            document.querySelector(".nameError").innerHTML = "";
+            document.querySelector(".subjectError").innerHTML = "";
+            document.querySelector(".msgError").innerHTML = "";
+        }
+        // TWO SUCCES CONDITIONAL
+        // NAME & SUBJECT SUCCESS, MSG FAILURE
+        else if (name.value != "" && subject.value != "" && msg.value == "") {
+            document.querySelector(".msgError").innerHTML = "El mensaje es requerido.";
+            document.querySelector(".nameError").innerHTML = "";
+            document.querySelector(".subjectError").innerHTML = "";
+            document.querySelector(".validate").innerHTML = "";
+        }
+        // NAME & MSG SUCCESS, SUBJECT FAILURE
+        else if (name.value != "" && subject.value == "" && msg.value !== "") {
+            document.querySelector(".subjectError").innerHTML = "El asunto es requerido.";
+            document.querySelector(".nameError").innerHTML = "";
+            document.querySelector(".msgError").innerHTML = "";
+            document.querySelector(".validate").innerHTML = "";
+        }
+        // SUBJECT & MSG SUCCESS, NAME FAILURE
+        else if (name.value == "" && subject.value != "" && msg.value !== "") {
+            document.querySelector(".nameError").innerHTML = "El nombre es requerido.";
+            document.querySelector(".subjectError").innerHTML = "";
+            document.querySelector(".msgError").innerHTML = "";
+            document.querySelector(".validate").innerHTML = "";
+        }
+        // ONE SUCCESS CONDITIONAL
+        // NAME SUCCESS, SUBJECT & MSG FAILURE
+        else if (name.value != "" && subject.value == "" && msg.value == "") {
+            document.querySelector(".nameError").innerHTML = "";
+            document.querySelector(".subjectError").innerHTML = "El asunto es requerido.";
+            document.querySelector(".msgError").innerHTML = "El mensaje es requerido.";
+            document.querySelector(".validate").innerHTML = "";
+        }
+        // SUBJECT SUCCESS, NAME & MSG FAILURE
+        else if (name.value == "" && subject.value != "" && msg.value == "") {
+            document.querySelector(".subjectError").innerHTML = "";
+            document.querySelector(".nameError").innerHTML = "El nombre es requerido.";
+            document.querySelector(".msgError").innerHTML = "El mensaje es requerido.";
+            document.querySelector(".validate").innerHTML = "";
+        }
+        // MSG SUCCESS, NAME & SUBJECT FAILURE
+        else if (name.value == "" && subject.value == "" && msg.value != "") {
+            document.querySelector(".msgError").innerHTML = "";
+            document.querySelector(".nameError").innerHTML = "El nombre es requerido.";
+            document.querySelector(".subjectError").innerHTML = "El asunto es requerido.";
+            document.querySelector(".validate").innerHTML = "";
+        }
+        // FULL FAILURE CASE
+        else {
+            document.querySelector(".nameError").innerHTML = "El nombre es requerido.";
+            document.querySelector(".subjectError").innerHTML = "El asunto es requerido.";
+            document.querySelector(".msgError").innerHTML = "El mensaje es requerido.";
+            document.querySelector(".validate").innerHTML = "";
+        }
+    } else {
+        alert("FIN");
     }
-    // NAME & MSG SUCCESS, SUBJECT FAILURE
-    else if (name.value != "" && subject.value == "" && msg.value !== "") {
-        document.querySelector(".subjectError").innerHTML = "El asunto es requerido.";
-        document.querySelector(".nameError").innerHTML = "";
-        document.querySelector(".msgError").innerHTML = "";
-        document.querySelector(".validate").innerHTML = "";
-    }
-    // SUBJECT & MSG SUCCESS, NAME FAILURE
-    else if (name.value == "" && subject.value != "" && msg.value !== "") {
-        document.querySelector(".nameError").innerHTML = "El nombre es requerido.";
-        document.querySelector(".subjectError").innerHTML = "";
-        document.querySelector(".msgError").innerHTML = "";
-        document.querySelector(".validate").innerHTML = "";
-    }
-    // ONE SUCCESS CONDITIONAL
-    // NAME SUCCESS, SUBJECT & MSG FAILURE
-    else if (name.value != "" && subject.value == "" && msg.value == "") {
-        document.querySelector(".nameError").innerHTML = "";
-        document.querySelector(".subjectError").innerHTML = "El asunto es requerido.";
-        document.querySelector(".msgError").innerHTML = "El mensaje es requerido.";
-        document.querySelector(".validate").innerHTML = "";
-    }
-    // SUBJECT SUCCESS, NAME & MSG FAILURE
-    else if (name.value == "" && subject.value != "" && msg.value == "") {
-        document.querySelector(".subjectError").innerHTML = "";
-        document.querySelector(".nameError").innerHTML = "El nombre es requerido.";
-        document.querySelector(".msgError").innerHTML = "El mensaje es requerido.";
-        document.querySelector(".validate").innerHTML = "";
-    }
-    // MSG SUCCESS, NAME & SUBJECT FAILURE
-    else if (name.value == "" && subject.value == "" && msg.value != "") {
-        document.querySelector(".msgError").innerHTML = "";
-        document.querySelector(".nameError").innerHTML = "El nombre es requerido.";
-        document.querySelector(".subjectError").innerHTML = "El asunto es requerido.";
-        document.querySelector(".validate").innerHTML = "";
-    }
-    // FULL FAILURE CASE
-    else {
-        document.querySelector(".nameError").innerHTML = "El nombre es requerido.";
-        document.querySelector(".subjectError").innerHTML = "El asunto es requerido.";
-        document.querySelector(".msgError").innerHTML = "El mensaje es requerido.";
-        document.querySelector(".validate").innerHTML = "";
-    }
+
 });
