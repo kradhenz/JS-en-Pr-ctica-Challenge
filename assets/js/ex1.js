fbaff1c56906d015e812e1f54cf03fcc63331c94
kradhenz <tomas25498@gmail.com>
2024-02-10 01:39:55 -0300

    :hammer: Code 'ejercicio1' improved and documentation writed :bulb:

--- assets/js/ex1.js
+++ assets/js/ex1.js
@@ -1,94 +1,47 @@
+// Remove all content in the fields
+function clean() {
+    document.querySelectorAll(".validate, .nameError, .subjectError, .msgError").forEach(element => {
+        element.innerHTML = "";
+    });
+};
+// Deliver a success message
+function success() {
+    document.querySelector(".validate").innerHTML = "¡Mensaje enviado con éxito!";
+};
+// Evalute if the input content match
+function exclude(input) {
+    return validate = /[a-zA-Z]/gim.test(input);
+}
+// Evalute the content to deliver a request or success message
 function validation(name, subject, msg) {
     let success = true;
-    let validate = /[a-zA-Z]/gim;
-
-    if (validate.test(name) == false) {
+    if (exclude(name) == false) {
         document.querySelector(".nameError").innerHTML = "Ingrese un nombre válido."
         success = false;
     };
-
-    if (validate.test(subject) == false) {
-        document.querySelector(".subjectError").innerHTML = "Ingrese un nombre válido."
+    if (exclude(subject) == false) {
+        document.querySelector(".subjectError").innerHTML = "Ingrese un asunto válido."
         success = false;
     };
-
-    if (validate.test(msg) == false) {
-        document.querySelector(".msgError").innerHTML = "Ingrese un nombre válido."
+    if (exclude(msg) == false) {
+        document.querySelector(".msgError").innerHTML = "Ingrese un mensaje válido."
         success = false;
     };
-
     return success;
 }
 /* 
     -->   START   <-- 
 */
-var myBtn = document.getElementById("send");
-myBtn.addEventListener('click', function () {
-    let name = document.querySelector("#name");
-    let subject = document.querySelector("#subject");
-    let msg = document.querySelector("#msg");
-
-    if (validation(name) == true) {
-        // FULL SUCCESS CASE
-        if (name.value != "" && subject.value != "" && msg.value != "") {
-            document.querySelector(".validate").innerHTML = "¡Mensaje enviado con éxito!";
-            document.querySelector(".nameError").innerHTML = "";
-            document.querySelector(".subjectError").innerHTML = "";
-            document.querySelector(".msgError").innerHTML = "";
-        }
-        // TWO SUCCES CONDITIONAL
-        // NAME & SUBJECT SUCCESS, MSG FAILURE
-        else if (name.value != "" && subject.value != "" && msg.value == "") {
-            document.querySelector(".msgError").innerHTML = "El mensaje es requerido.";
-            document.querySelector(".nameError").innerHTML = "";
-            document.querySelector(".subjectError").innerHTML = "";
-            document.querySelector(".validate").innerHTML = "";
-        }
-        // NAME & MSG SUCCESS, SUBJECT FAILURE
-        else if (name.value != "" && subject.value == "" && msg.value !== "") {
-            document.querySelector(".subjectError").innerHTML = "El asunto es requerido.";
-            document.querySelector(".nameError").innerHTML = "";
-            document.querySelector(".msgError").innerHTML = "";
-            document.querySelector(".validate").innerHTML = "";
-        }
-        // SUBJECT & MSG SUCCESS, NAME FAILURE
-        else if (name.value == "" && subject.value != "" && msg.value !== "") {
-            document.querySelector(".nameError").innerHTML = "El nombre es requerido.";
-            document.querySelector(".subjectError").innerHTML = "";
-            document.querySelector(".msgError").innerHTML = "";
-            document.querySelector(".validate").innerHTML = "";
-        }
-        // ONE SUCCESS CONDITIONAL
-        // NAME SUCCESS, SUBJECT & MSG FAILURE
-        else if (name.value != "" && subject.value == "" && msg.value == "") {
-            document.querySelector(".nameError").innerHTML = "";
-            document.querySelector(".subjectError").innerHTML = "El asunto es requerido.";
-            document.querySelector(".msgError").innerHTML = "El mensaje es requerido.";
-            document.querySelector(".validate").innerHTML = "";
-        }
-        // SUBJECT SUCCESS, NAME & MSG FAILURE
-        else if (name.value == "" && subject.value != "" && msg.value == "") {
-            document.querySelector(".subjectError").innerHTML = "";
-            document.querySelector(".nameError").innerHTML = "El nombre es requerido.";
-            document.querySelector(".msgError").innerHTML = "El mensaje es requerido.";
-            document.querySelector(".validate").innerHTML = "";
-        }
-        // MSG SUCCESS, NAME & SUBJECT FAILURE
-        else if (name.value == "" && subject.value == "" && msg.value != "") {
-            document.querySelector(".msgError").innerHTML = "";
-            document.querySelector(".nameError").innerHTML = "El nombre es requerido.";
-            document.querySelector(".subjectError").innerHTML = "El asunto es requerido.";
-            document.querySelector(".validate").innerHTML = "";
-        }
-        // FULL FAILURE CASE
-        else {
-            document.querySelector(".nameError").innerHTML = "El nombre es requerido.";
-            document.querySelector(".subjectError").innerHTML = "El asunto es requerido.";
-            document.querySelector(".msgError").innerHTML = "El mensaje es requerido.";
-            document.querySelector(".validate").innerHTML = "";
-        }
-    } else {
-        alert("FIN");
-    }
-
+// Get the form elements and add a submit event
+document.getElementById("forms").addEventListener('submit', function (event) {
+    event.preventDefault();
+    clean();
+    // Select the attribute in 'value=' to evaluate and modify if it apply
+    const name = document.querySelector("#name").value;
+    const subject = document.querySelector("#subject").value;
+    const msg = document.querySelector("#msg").value;
+    // Conditional to show a message
+    if (validation(name, subject, msg)) {
+        success();
+    };
 });
--- assets/js/ex2.js
+++ assets/js/ex2.js
@@ -1,6 +1,6 @@
 // get the elements in palette id div
 document.getElementById("palette").addEventListener('click', function(event) {
-    // verify button clicked to find a match
+    // verify button clicked to find a match using target property
     if (event.target && event.target.matches("button")) {
         // change background color based in button background color matched
         document.getElementById("box").style.backgroundColor = event.target.style.backgroundColor;
